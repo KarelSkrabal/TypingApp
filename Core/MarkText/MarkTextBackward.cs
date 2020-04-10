7b93b2ea421669c8d18e025c0752da8196d4195e
KarelSkrabal <karel.skrabal@seznam.cz>
2020-04-10 21:22:09 +0200

    Initial commit

--- Core/MarkText/MarkTextBackward.cs
+++ Core/MarkText/MarkTextBackward.cs
@@ -9,14 +9,17 @@ namespace TypingAppWF
         public void MarkText(RichTextBox richTextBox, ref int position)
         {
             var lessonText = richTextBox.Text;
-            var s1 = lessonText.Substring(0, position - 1);
-            var s2 = lessonText[position - 1].ToString();//this letter will be colored
-            var s3 = lessonText.Substring(position);
-            richTextBox.Text = string.Empty;
-            richTextBox.AppendText(s1);
-            richTextBox.AppendText(s2, Color.Red);
-            richTextBox.AppendText(s3);
-            position--;
+            if(position > 0)
+            {
+                var s1 = lessonText.Substring(0, position - 1);
+                var s2 = lessonText[position - 1].ToString();//this letter will be colored
+                var s3 = lessonText.Substring(position);
+                richTextBox.Text = string.Empty;
+                richTextBox.AppendText(s1);
+                richTextBox.AppendText(s2, Color.Red);
+                richTextBox.AppendText(s3);
+                position--;
+            }
         }
     }
 }
